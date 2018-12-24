ccd3bfeaf35cd6f92b57ba542cf1c8d30cc5cf5f
Marton Helli <rdhelli@gmail.com>
2018-12-24 08:29:12 +0100

    project commit

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -212,13 +212,13 @@ std::vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
           // solution.x[cte_start + 1], solution.x[epsi_start + 1],
           // solution.x[delta_start],   solution.x[a_start]};
   
-  vector<double> result;
-  result.push_back(solution.x[delta_start]);
-  result.push_back(solution.x[a_start]);
+  vector<double> sol;
+  sol.push_back(solution.x[delta_start]);
+  sol.push_back(solution.x[a_start]);
   for (int i = 0; i < N-1; i++) {
-    result.push_back(solution.x[x_start + i + 1]);
-    result.push_back(solution.x[y_start + i + 1]);
+    sol.push_back(solution.x[x_start + i + 1]);
+    sol.push_back(solution.x[y_start + i + 1]);
   }
   
-  return result;
+  return sol;
 }
